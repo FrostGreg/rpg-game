15138df8a6c00a71a1ef2a0178db78951ca2bc96
Greg Frost <gregfrost305@gmail.com>
2021-07-08 13:15:13 +0100

    Improvement to design
    - Tkinter implementation encapsulated in interface class
    - interface class constructor only takes game object

--- classes/interface.py
+++ classes/interface.py
@@ -4,47 +4,58 @@ from time import sleep
 
 
 class Interface:
-    def __init__(self, game_obj, root, health, enemy_health, style, canvas, mp_lbl, health_lbl, enemy_health_lbl,
-                 magic_points, sprite, boss, win_pic, lose_pic, fire_pic, lightning_pic):
+    def __init__(self, game):
 
-        self.root = root
-        self.health = health
-        self.enemy_health = enemy_health
-        self.style = style
-        self.canvas = canvas
-        self.mp_lbl = mp_lbl
-        self.health_lbl = health_lbl
-        self.enemy_health_lbl = enemy_health_lbl
-        self.magic_points = magic_points
-        self.sprite = sprite
-        self.boss = boss
-        self.win_pic = win_pic
-        self.lose_pic = lose_pic
-        self.fire_pic = fire_pic
-        self.lightning_pic = lightning_pic
-
-        self.game_obj = game_obj
+        self.game = game
         self.player_choice = 0
         self.menu_level = 0
         self.animation_progress = False
         self.running = True
 
-        self.btn_tl = ttk.Button(root, style="TButton", text="Melee", command=lambda: self.get_choice(1))
+        self.root = tk.Tk()
+        self.health = ttk.Progressbar(self.root, style="green.Horizontal.TProgressbar",
+                                      orient=tk.HORIZONTAL, length=100, mode="determinate")
+        self.enemy_health = ttk.Progressbar(self.root, style="red.Horizontal.TProgressbar",
+                                            orient=tk.HORIZONTAL, length=250, mode="determinate")
+        self.style = ttk.Style()
+        self.canvas = tk.Canvas(self.root, width=800, height=350, bg="red")
+        self.mp_lbl = tk.Label(self.root,
+                               text="Player MP:" + str(self.game.player.get_mp()) + "/" + str(
+                                   self.game.player.get_max_mp()))
+        self.health_lbl = tk.Label(self.root, text="Player HP:" + str(self.game.player.get_hp()) + "/"
+                                                   + str(self.game.player.get_max_hp()))
+        self.enemy_health_lbl = tk.Label(self.root,
+                                         text="Enemy HP:" + str(self.game.enemy.get_hp()) + "/" + str(
+                                             self.game.enemy.get_max_hp()))
+        self.magic_points = ttk.Progressbar(self.root, style="blue.Horizontal.TProgressbar",
+                                            orient=tk.HORIZONTAL, length=100, mode="determinate")
+
+        self.win_pic = tk.PhotoImage(file=self.game.dir + "WIN.png")
+        self.lose_pic = tk.PhotoImage(file=self.game.dir + "LOSE.png")
+        self.fire_pic = tk.PhotoImage(file=self.game.dir + "fire.png")
+        self.lightning_pic = tk.PhotoImage(file=self.game.dir + "lightning2.png")
+
+        self.background = tk.PhotoImage(file=self.game.dir + "back.png")
+        self.sprite_pic = tk.PhotoImage(file=self.game.dir + "player.png")
+        self.boss_pic = tk.PhotoImage(file=self.game.dir + "orc.png")
+        self.sprite = self.boss = None
+
+        self.btn_tl = ttk.Button(self.root, style="TButton", text="Melee", command=lambda: self.get_choice(1))
         self.btn_tl.grid(column=0, row=5)
 
-        self.btn_tr = ttk.Button(root, style="TButton", text="Magic", command=lambda: self.get_choice(2))
+        self.btn_tr = ttk.Button(self.root, style="TButton", text="Magic", command=lambda: self.get_choice(2))
         self.btn_tr.grid(column=1, row=5)
 
-        self.btn_l = ttk.Button(root, style="TButton", text="Items", command=lambda: self.get_choice(3))
+        self.btn_l = ttk.Button(self.root, style="TButton", text="Items", command=lambda: self.get_choice(3))
         self.btn_l.grid(column=0, row=6)
 
-        self.btn_r = ttk.Button(root, style="TButton", command=lambda: self.get_choice(4))
+        self.btn_r = ttk.Button(self.root, style="TButton", command=lambda: self.get_choice(4))
         self.btn_r.grid(column=1, row=6)
 
-        self.btn_bl = ttk.Button(root, style="TButton", command=lambda: self.get_choice(5))
+        self.btn_bl = ttk.Button(self.root, style="TButton", command=lambda: self.get_choice(5))
         self.btn_bl.grid(column=0, row=7)
 
-        self.btn_br = ttk.Button(root, style="TButton", command=lambda: self.get_choice(6))
+        self.btn_br = ttk.Button(self.root, style="TButton", command=lambda: self.get_choice(6))
         self.btn_br.grid(column=1, row=7)
 
     def display_menu(self):
@@ -68,11 +79,11 @@ class Interface:
     def display_items(self):
         self.menu_level = 2
         self.btn_tl.configure(text="Go back")
-        self.btn_tr.configure(text="Potion (x" + str(self.game_obj.player.items[0]["quantity"]) + ")")
-        self.btn_l.configure(text="Hi-Potion (x" + str(self.game_obj.player.items[1]["quantity"]) + ")")
-        self.btn_r.configure(text="elixir (x" + str(self.game_obj.player.items[2]["quantity"]) + ")")
-        self.btn_bl.configure(text="Splash elixir (x" + str(self.game_obj.player.items[3]["quantity"]) + ")")
-        self.btn_br.configure(text="Bomb (x" + str(self.game_obj.player.items[4]["quantity"]) + ")")
+        self.btn_tr.configure(text="Potion (x" + str(self.game.player.items[0]["quantity"]) + ")")
+        self.btn_l.configure(text="Hi-Potion (x" + str(self.game.player.items[1]["quantity"]) + ")")
+        self.btn_r.configure(text="elixir (x" + str(self.game.player.items[2]["quantity"]) + ")")
+        self.btn_bl.configure(text="Splash elixir (x" + str(self.game.player.items[3]["quantity"]) + ")")
+        self.btn_br.configure(text="Bomb (x" + str(self.game.player.items[4]["quantity"]) + ")")
 
     def get_choice(self, i):
         if self.running:
@@ -86,11 +97,11 @@ class Interface:
 
     def choose_action(self):
         if self.player_choice == 1:
-            dmg = self.game_obj.player.generate_damage()
-            self.game_obj.enemy.take_damage(dmg)
+            dmg = self.game.player.generate_damage()
+            self.game.enemy.take_damage(dmg)
             self.player_attack_anim()
-            self.update_health(self.game_obj.enemy)
-            self.check_health(self.game_obj.enemy)
+            self.update_health(self.game.enemy)
+            self.check_health(self.game.enemy)
             self.enemy_attack()
         elif self.player_choice == 2:
             self.display_magic()
@@ -104,20 +115,20 @@ class Interface:
             self.display_menu()
             return
         elif self.player_choice == 6:
-            self.game_obj.player.heal(self.game_obj.heal.dmg)
-            spell_cost = self.game_obj.player.magic[4].cost
+            self.game.player.heal(self.game.heal.dmg)
+            spell_cost = self.game.player.magic[4].cost
         else:
-            magic_dmg = self.game_obj.player.magic[self.player_choice - 2].generate_spell_damage()
-            spell_cost = self.game_obj.player.magic[self.player_choice - 2].cost
-            name = self.game_obj.player.magic[self.player_choice - 2].name
+            magic_dmg = self.game.player.magic[self.player_choice - 2].generate_spell_damage()
+            spell_cost = self.game.player.magic[self.player_choice - 2].cost
+            name = self.game.player.magic[self.player_choice - 2].name
             self.magic_animation(name)
 
-        self.game_obj.player.reduce_mp(spell_cost)
+        self.game.player.reduce_mp(spell_cost)
         self.update_mp()
-        self.game_obj.enemy.take_damage(magic_dmg)
-        self.update_health(self.game_obj.enemy)
+        self.game.enemy.take_damage(magic_dmg)
+        self.update_health(self.game.enemy)
 
-        self.check_health(self.game_obj.enemy)
+        self.check_health(self.game.enemy)
         self.enemy_attack()
 
     def choose_item(self):
@@ -125,40 +136,40 @@ class Interface:
             self.display_menu()
             return
         else:
-            item = self.game_obj.player.items[self.player_choice - 2]["item"]
-            if self.game_obj.player.items[self.player_choice - 2]["quantity"] > 0:
+            item = self.game.player.items[self.player_choice - 2]["item"]
+            if self.game.player.items[self.player_choice - 2]["quantity"] > 0:
                 dmg = item.prop
                 if item.form == "potion":
-                    self.game_obj.player.heal(dmg)
-                    print(self.game_obj.player.get_hp())
+                    self.game.player.heal(dmg)
+                    print(self.game.player.get_hp())
                 elif item.form == "elixir":
-                    self.game_obj.player.mp += dmg
-                    print(self.game_obj.player.get_mp())
+                    self.game.player.mp += dmg
+                    print(self.game.player.get_mp())
                 elif item.form == "attack":
-                    self.game_obj.enemy.take_damage(dmg)
-                    self.update_health(self.game_obj.enemy)
-                    self.check_health(self.game_obj.enemy)
+                    self.game.enemy.take_damage(dmg)
+                    self.update_health(self.game.enemy)
+                    self.check_health(self.game.enemy)
 
-                self.game_obj.player.items[self.player_choice - 2]["quantity"] -= 1
+                self.game.player.items[self.player_choice - 2]["quantity"] -= 1
             else:
                 pass
 
         self.enemy_attack()
 
     def enemy_attack(self):
-        if self.game_obj.enemy.get_hp() > 0:
-            enemy_dmg = self.game_obj.enemy.generate_damage()
+        if self.game.enemy.get_hp() > 0:
+            enemy_dmg = self.game.enemy.generate_damage()
             self.enemy_attack_anim()
-            self.game_obj.player.take_damage(enemy_dmg)
-            self.update_health(self.game_obj.player)
-            self.check_health(self.game_obj.player)
+            self.game.player.take_damage(enemy_dmg)
+            self.update_health(self.game.player)
+            self.check_health(self.game.player)
         else:
             pass
 
     def update_health(self, character):
         hp = character.get_hp()
         max_hp = character.get_max_hp()
-        if character == self.game_obj.player:
+        if character == self.game.player:
             self.health['value'] = hp
             self.health_lbl.configure(text="Player HP:" + str(hp) + "/" + str(max_hp))
             self.health.update()
@@ -168,8 +179,8 @@ class Interface:
             self.enemy_health.update()
 
     def update_mp(self):
-        mp = self.game_obj.player.get_mp()
-        max_mp = self.game_obj.player.get_max_mp()
+        mp = self.game.player.get_mp()
+        max_mp = self.game.player.get_max_mp()
 
         self.magic_points['value'] = mp
         self.mp_lbl.configure(text="Player MP:" + str(mp) + "/" + str(max_mp))
@@ -199,12 +210,12 @@ class Interface:
 
     def check_health(self, character):
         hp = character.get_hp()
-        if character == self.game_obj.enemy:
+        if character == self.game.enemy:
             if hp == 0:
                 self.canvas.create_image(200, 100, image=self.win_pic, anchor=tk.NW)
                 self.running = False
                 self.canvas.delete(self.boss)
-        elif character == self.game_obj.player:
+        elif character == self.game.player:
             if hp == 0:
                 self.canvas.create_image(200, 100, image=self.lose_pic, anchor=tk.NW)
                 self.running = False
@@ -234,3 +245,51 @@ class Interface:
                 self.canvas.update()
                 sleep(0.05)
             x1 += 30
+
+    def run(self):
+
+        self.root.geometry("805x650")
+        self.root.title("Dungeon Quest")
+        self.root.configure(bg="darkgrey")
+
+        self.style.theme_use('clam')
+        self.style.configure("red.Horizontal.TProgressbar", foreground='red', background='red')
+        self.style.configure("green.Horizontal.TProgressbar", foreground='green', background='green')
+        self.style.configure("blue.Horizontal.TProgressbar", foreground='blue', background='blue')
+
+        self.style.configure('TButton',
+                             font=('calibri', 10),
+                             borderwidth='4',
+                             background="#002366",
+                             foreground="white",
+                             height=4, width=54)
+
+        self.style.map('TButton',
+                       foreground=[('active', 'white')],
+                       background=[('active', "#021A44")])
+
+        # region "canvas + buttons"
+
+        self.canvas.grid(column=0, row=0, columnspan=2)
+
+        self.canvas.create_image(2, 0, image=self.background, anchor=tk.NW)
+
+        self.sprite = self.canvas.create_image(100, 175, image=self.sprite_pic, anchor=tk.NW)
+        self.boss = self.canvas.create_image(600, 100, image=self.boss_pic, anchor=tk.NW)
+
+        self.health['value'] = 100
+        self.health.grid(column=0, row=2, sticky=tk.W)
+
+        self.health_lbl.grid(column=0, row=1, sticky=tk.W)
+
+        self.magic_points['value'] = 100
+        self.magic_points.grid(column=0, row=4, sticky=tk.W)
+
+        self.mp_lbl.grid(column=0, row=3, sticky=tk.W)
+
+        self.enemy_health['value'] = 250
+        self.enemy_health.grid(column=1, row=2, sticky=tk.E)
+
+        self.enemy_health_lbl.grid(column=1, row=1, sticky=tk.E)
+
+        self.root.mainloop()
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import tkinter as tk
-from tkinter import ttk
 from classes.interface import Interface
 from classes.game import Game
 
-game = Game()
 valid = False
 game_modes = [1, 2]
 mode_select = 0
@@ -21,71 +18,11 @@ while not valid:
     else:
         print("Invalid game mode Try Again")
 
+game = Game()
+
 if mode_select == 1:
     game.run()
 
 elif mode_select == 2:
-    root = tk.Tk()
-    root.geometry("805x650")
-    root.title("Dungeon Quest")
-    root.configure(bg="darkgrey")
-
-    style = ttk.Style()
-    style.theme_use('clam')
-    style.configure("red.Horizontal.TProgressbar", foreground='red', background='red')
-    style.configure("green.Horizontal.TProgressbar", foreground='green', background='green')
-    style.configure("blue.Horizontal.TProgressbar", foreground='blue', background='blue')
-
-    style.configure('TButton',
-                    font=('calibri', 10),
-                    borderwidth='4',
-                    background="#002366",
-                    foreground="white",
-                    height=4, width=54)
-
-    style.map('TButton',
-              foreground=[('active', 'white')],
-              background=[('active', "#021A44")])
-
-    # region "canvas + buttons"
-    canvas = tk.Canvas(root, width=800, height=350, bg="red")
-    canvas.grid(column=0, row=0, columnspan=2)
-    dir = "docs/assets/"
-    background = tk.PhotoImage(file=dir + "back.png")
-    sprite_pic = tk.PhotoImage(file=dir + "player.png")
-    boss_pic = tk.PhotoImage(file=dir + "orc.png")
-    fire_pic = tk.PhotoImage(file=dir + "fire.png")
-    lightning_pic = tk.PhotoImage(file=dir + "lightning2.png")
-    win_pic = tk.PhotoImage(file=dir + "WIN.png")
-    lose_pic = tk.PhotoImage(file=dir + "LOSE.png")
-    canvas.create_image(2, 0, image=background, anchor=tk.NW)
-
-    boss = canvas.create_image(600, 100, image=boss_pic, anchor=tk.NW)
-    sprite = canvas.create_image(100, 175, image=sprite_pic, anchor=tk.NW)
-
-    health = ttk.Progressbar(root, style="green.Horizontal.TProgressbar",
-                             orient=tk.HORIZONTAL, length=100, mode="determinate")
-    health['value'] = 100
-    health.grid(column=0, row=2, sticky=tk.W)
-    health_lbl = tk.Label(root, text="Player HP:" + str(game.player.get_hp()) + "/" + str(game.player.get_max_hp()))
-    health_lbl.grid(column=0, row=1, sticky=tk.W)
-
-    magic_points = ttk.Progressbar(root, style="blue.Horizontal.TProgressbar",
-                                   orient=tk.HORIZONTAL, length=100, mode="determinate")
-    magic_points['value'] = 100
-    magic_points.grid(column=0, row=4, sticky=tk.W)
-    mp_lbl = tk.Label(root, text="Player MP:" + str(game.player.get_mp()) + "/" + str(game.player.get_max_mp()))
-    mp_lbl.grid(column=0, row=3, sticky=tk.W)
-
-    enemy_health = ttk.Progressbar(root, style="red.Horizontal.TProgressbar",
-                                   orient=tk.HORIZONTAL, length=250, mode="determinate")
-    enemy_health['value'] = 250
-    enemy_health.grid(column=1, row=2, sticky=tk.E)
-    enemy_health_lbl = tk.Label(root, text="Enemy HP:" + str(game.enemy.get_hp()) + "/" + str(game.enemy.get_max_hp()))
-    enemy_health_lbl.grid(column=1, row=1, sticky=tk.E)
-
-    buttons = Interface(game, root, health, enemy_health, style, canvas, mp_lbl, health_lbl, enemy_health_lbl,
-                        magic_points, sprite, boss, win_pic, lose_pic, fire_pic, lightning_pic)
-    # endregion
-
-    root.mainloop()
+    gui = Interface(game)
+    gui.run()
